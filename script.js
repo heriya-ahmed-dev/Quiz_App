a1618c4b52bacd7a74c1f9ec1299d4425e8f071b
Heracitta <heriyaahmedsiraje@gmail.com>
2026-06-21 13:02:22 +0300

    data added

---
+++ script.js
@@ -0,0 +1,48 @@
+const data = [
+    {
+        question1:'1. Who was the first adult male to accept Islam?',
+        option1 :'def',
+        option2:'func',
+        option3 :'define',
+        option4 :'function',
+        answer : 'def'
+
+    },
+     {
+        question2:'2. What is the output of print(2 ** 3)?',
+        option1 :'6',
+        option2:'9',
+        option3 :'8',
+        option4 :'5',
+        answer : '8'
+
+    },
+     {
+        question3:'3. Which data type is immutable?',
+        options :'list',
+        option2:'dictionary',
+        option3 :'set',
+        option4 :'tuple',
+        answer : 'tuple'
+
+    },
+     {
+        question4:'4. Which method adds an item to the end of a list in Python?',
+        option1 :'add()',
+        option2:'pop()',
+        option3 :'insert()',
+        option4 :'append()',
+        answer : 'append()'
+
+    },
+     {
+        question5:'5. Which keyword is used to create a class in Python?',
+        option1 :'object',
+        option2:'class',
+        option3 :'define',
+        option4 :'sttruct',
+        answer : 'class'
+
+    }
+]
+
